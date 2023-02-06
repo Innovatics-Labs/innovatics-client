7ae46f0bd950e518bb23ce5012f2543bd8296152
AOMuiz <abdulwasiumuiz@gmail.com>
2023-02-06 10:48:19 +0100

    extract Topic card to its own component

---
+++ src/components/TopicCard.js
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import { BsSun } from "react-icons/bs";
+import styled from "styled-components";
+
+import arrow from "../../assets/images/arrow.png";
+import GradientIcon from "../GradientIcon";
+
+const TopicCard = ({ color, number, comingSoon, topic }) => {
+  return (
+    <Card>
+      <GradientContainer>
+        <GradientIcon
+          IconComponent={<BsSun size={30} color={color} />}
+          bgColor={color}
+        />
+        <Number color={color}>{number}</Number>
+      </GradientContainer>
+      <Topic>{topic}</Topic>
+      <Arrow>
+        <Image src={arrow} alt={""} />
+        {comingSoon && <p>Coming Soon</p>}
+      </Arrow>
+    </Card>
+  );
+};
+
+export default TopicCard;
+
+const Card = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  padding: 2.5rem;
+  background: linear-gradient(rgba(13, 17, 22), rgba(13, 17, 22, 0.8));
+  border-radius: 9px;
+  gap: 4rem;
+  flex: 1;
+  filter: blur(0.48px) drop-shadow(0px 0px 57px rgba(62, 63, 73, 0.25));
+`;
+
+const GradientContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Topic = styled.p`
+  font-weight: 500;
+  font-size: 28px;
+  color: white;
+  margin: auto 0;
+  width: 50%;
+  line-height: 40px;
+`;
+
+const Number = styled.p`
+  font-weight: 400;
+  font-size: 24px;
+  color: ${({ color }) => color};
+  margin-block: 0px;
+`;
+
+const Arrow = styled.div`
+  svg {
+    font-weight: 200;
+    stroke-width: 10px;
+    width: 100%;
+  }
+`;
--- src/components/home/LearningsSections.js
+++ src/components/home/LearningsSections.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { BsSun } from "react-icons/bs";
-import Image from "next/image";
 
 import LineGradient from "../LineGradient";
 import GradientIcon from "../GradientIcon";
 import { TextWithColor } from "./Practicality";
-import arrow from "../../assets/images/arrow.png";
+import TopicCard from "../TopicCard";
 
 const LearningsSections = () => {
   return (
@@ -30,58 +29,14 @@ const LearningsSections = () => {
         </TextContent>
       </TopSection>
       <BottomSection>
-        <Card>
-          <GradientContainer>
-            <GradientIcon
-              IconComponent={<BsSun size={30} color="#44E986" />}
-              bgColor={"#44E986"}
-            />
-            <Number color="#44E986">01</Number>
-          </GradientContainer>
-          <Topic>Data Science</Topic>
-          <Arrow>
-            <Image src={arrow} alt={""} />
-          </Arrow>
-        </Card>
-        <Card>
-          <GradientContainer>
-            <GradientIcon
-              IconComponent={<BsSun size={30} color="#FFA28B" />}
-              bgColor={"#FFA28B"}
-            />
-            <Number color="#FFA28B">02</Number>
-          </GradientContainer>
-          <Topic>Software Engineering </Topic>
-          <Arrow>
-            <Image src={arrow} alt={""} />
-          </Arrow>
-        </Card>
-        <Card>
-          <GradientContainer>
-            <GradientIcon
-              IconComponent={<BsSun size={30} color="#DD7DF7" />}
-              bgColor={"#DD7DF7"}
-            />
-            <Number color="#DD7DF7">03</Number>
-          </GradientContainer>
-          <Topic>Cyber Security</Topic>
-          <Arrow>
-            <Image src={arrow} alt={""} />
-          </Arrow>
-        </Card>
-        <Card>
-          <GradientContainer>
-            <GradientIcon
-              IconComponent={<BsSun size={30} color="#8B90FF" />}
-              bgColor={"#8B90FF"}
-            />
-            <Number color="#8B90FF">04</Number>
-          </GradientContainer>
-          <Topic>Cloud Engineering</Topic>
-          <Arrow>
-            <Image src={arrow} alt={""} />
-          </Arrow>
-        </Card>
+        <TopicCard topic={"Data Science"} color="#44E986" number={"01"} />
+        <TopicCard
+          topic={"Software Engineering"}
+          color="#FFA28B"
+          number={"02"}
+        />
+        <TopicCard topic={"Cyber Security"} color="#DD7DF7" number={"03"} />
+        <TopicCard topic={"Cloud Engineering"} color="#8B90FF" number={"04"} />
       </BottomSection>
     </Container>
   );
@@ -130,46 +85,4 @@ const BottomSection = styled.div`
   /* :not(:first-child) {
     margin-left: -50px;
   } */
-`;
-
-const Card = styled.div`
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  padding: 2.5rem;
-  background: linear-gradient(rgba(13, 17, 22), rgba(13, 17, 22, 0.8));
-  border-radius: 9px;
-  gap: 4rem;
-  flex: 1;
-  filter: blur(0.48px) drop-shadow(0px 0px 57px rgba(62, 63, 73, 0.25));
-`;
-
-const GradientContainer = styled.div`
-  display: flex;
-  gap: 2rem;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Topic = styled.p`
-  font-weight: 500;
-  font-size: 28px;
-  color: white;
-  margin: auto 0;
-  width: 50%;
-  line-height: 40px;
-`;
-
-const Number = styled.p`
-  font-weight: 400;
-  font-size: 24px;
-  color: ${({ color }) => color};
-  margin-block: 0px;
-`;
-
-const Arrow = styled.div`
-  svg {
-    font-weight: 200;
-    stroke-width: 10px;
-    width: 100%;
-  }
 `;
