099b31971409c72a96930d4b88976bf5a5ff1b87
AOMuiz <abdulwasiumuiz@gmail.com>
2023-04-10 22:13:18 +0100

    restore data.js file for career outcome

---
+++ src/pages/bootcamps/data.js
@@ -0,0 +1,31 @@
+import { BsCollectionPlay } from "react-icons/bs";
+import { IoBookOutline } from "react-icons/io5";
+import { BiSupport } from "react-icons/bi";
+import { RiStackLine } from "react-icons/ri";
+
+export const careerOutcome = [
+  {
+    title: "Data Scientist",
+    subTitle:
+      "They use their skills in data analysis, statistical modeling, and machine learning to solve complex business problems and drive decision-making.",
+    icon: <BsCollectionPlay size={24} />,
+  },
+  {
+    title: "Machine Learning Engineer",
+    subTitle:
+      "Our graduates have landed roles as machine learning engineers, where they develop and deploy machine learning models at scale.",
+    icon: <RiStackLine size={24} />,
+  },
+  {
+    title: "Data Analyst",
+    subTitle:
+      "Our bootcamp provides a solid foundation in data analysis, which prepares our graduates for roles as data analysts.",
+    icon: <BiSupport size={24} />,
+  },
+  {
+    title: "Business Analyst",
+    subTitle:
+      "They use their skills in data analysis, project management, and strategic planning to drive business growth and improve organizational efficiency.",
+    icon: <IoBookOutline size={24} />,
+  },
+];
