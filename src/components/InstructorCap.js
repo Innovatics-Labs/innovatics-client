059249af62480f014d3d8a4e2984555b67ccaf4c
eniolastyle <amiolastyle@gmail.com>
2023-07-31 15:59:35 +0100

    updated instructors details

--- src/components/InstructorCap.js
+++ src/components/InstructorCap.js
@@ -1,10 +1,11 @@
-import { FaGraduationCap } from "react-icons/fa";
-import styled from "styled-components";
+import { FaGraduationCap } from 'react-icons/fa';
+import styled from 'styled-components';
 
-const InstructorCap = ({ size, iconsize = 50 }) => {
+const InstructorCap = ({ size, image, iconsize = 70 }) => {
   return (
     <Cap size={size}>
-      <FaGraduationCap color="white" size={iconsize} />
+      <FaGraduationCap color='white' size={iconsize} />
+      {/* <InstructorImage src={image} /> */}
     </Cap>
   );
 };
@@ -15,9 +16,16 @@ const Cap = styled.div`
   background: linear-gradient(90deg, #44e986 0%, #e94444 100%);
   border: 5px solid #f6f6f6;
   border-radius: 9999999px;
-  width: ${({ size }) => (size ? size : "100px")};
-  height: ${({ size }) => (size ? size : "100px")};
+  width: ${({ size }) => (size ? size : '100px')};
+  height: ${({ size }) => (size ? size : '100px')};
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const InstructorImage = styled.img`
+  height: 80px;
+  width: 80px;
+  object-fit: cover;
+  border-radius: 50%;
 `;
